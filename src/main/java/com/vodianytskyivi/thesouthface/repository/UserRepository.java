ff692f8b619c05222514d2c47f7745fa4f622ac4
Vodianytskyi Vladislav <vodianytsv@ae.com>
2020-06-30 12:57:27 +0300

    fb-vv-1 user editor implemented

--- src/main/java/com/vodianytskyivi/thesouthface/repository/UserRepository.java
+++ src/main/java/com/vodianytskyivi/thesouthface/repository/UserRepository.java
@@ -3,7 +3,7 @@ package com.vodianytskyivi.thesouthface.repository;
 import com.vodianytskyivi.thesouthface.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
     User findByUsername(String username);
 }
